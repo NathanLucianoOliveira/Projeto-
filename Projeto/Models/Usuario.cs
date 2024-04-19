69a6669f9ae09dac64c1aa8445518fdd5df1af4a
Miguel Oliveira <migueloliveiradasneves@gmail.com>
2024-04-19 20:03:04 -0300

    modelos backend

---
+++ Projeto/Models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Projeto.Models
+{
+    public class Usuario
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; } = "";
+        public string Sobrenome { get; set; } = "";
+        public string Imagem { get; set; } = "";
+        public string Email { get; set; } = "";
+        [JsonIgnore]
+        public string Senha { get; set; } = "";
+
+        public List<Grupo> Grupos { get; set; } = [];
+        public List<Participante> GruposParticipante { get; set; } = [];
+    }
+}
